a92f47b4f1fc40191512f1b296a137659393a0cb
joel <joeloregonmines@gmail.com>
2017-12-10 05:43:59 -0800

    Push to all 3 pools

--- README.md
+++ README.md
@@ -13,11 +13,17 @@ Install pssh: sudo apt install pssh
 
 #How to use:
 
-Due to naming conflicts with other Ubuntu packages you have to use "parallel-ssh" instead of "pssh"
+Due to naming conflicts with other Ubuntu packages you have to use "parallel-ssh" instead of "pssh"  
 
-parallel-ssh -h ./<nameofiplist>.<fileextension> -l root -A -P -I < ./<nameofcommandfile>.<fileextension> 
+parallel-ssh -h ./<nameofiplist>.<fileextension> -l root -A -P -I < ./<nameofcommandfile>.<fileextension>  
 parallel-ssh -h ./iplist.txt -l root -A -P -I < ./command.sh 
 
 #To do:  
 
-Possibly incorporate into a small docker-compose Linux image
+Give cgminer.conf.backup a date prefix for better tracking of changes
+
+Use vars in command file for url, worker, pass
+
+Possibly incorporate into a small docker-compose Linux image  
+
+Possibly pull client name from Sheets to build iplist. Could make use of command line arguments.  
--- command.sh
+++ command.sh
@@ -1,4 +1,18 @@
 cd /config
 cp ./cgminer.conf ./cgminer.conf.
 echo "Backup of cgminer.conf created"
+
+#first pool
+sed -i "s/$(grep -m 1 "url" cgminer.conf)/$(echo '"user": "derp",')/g" cgminer.conf
 sed -i "s/$(grep -m 1 "user" cgminer.conf)/$(echo '"user": "derp",')/g" cgminer.conf
+sed -i "s/$(grep -m 1 "password" cgminer.conf)/$(echo '"user": "derp",')/g" cgminer.conf
+
+#second pool
+sed -i "s/$(grep -m 2 "url" cgminer.conf)/$(echo '"user": "derp",')/g" cgminer.conf
+sed -i "s/$(grep -m 2 "user" cgminer.conf)/$(echo '"user": "derp",')/g" cgminer.conf
+sed -i "s/$(grep -m 2 "password" cgminer.conf)/$(echo '"user": "derp",')/g" cgminer.conf
+
+#third pool
+sed -i "s/$(grep -m 3 "url" cgminer.conf)/$(echo '"user": "derp",')/g" cgminer.conf
+sed -i "s/$(grep -m 3 "user" cgminer.conf)/$(echo '"user": "derp",')/g" cgminer.conf
+sed -i "s/$(grep -m 3 "password" cgminer.conf)/$(echo '"user": "derp",')/g" cgminer.conf
